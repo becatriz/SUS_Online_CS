da46b6c84661b210823bfdc1d78ab86ac51149a6
Rebeca Lopes <becatriz7@gmail.com>
2019-05-14 21:18:17 -0300

    Sprint 3

---
+++ src/br/com/sus_online/controller/AjaxApp.java
@@ -0,0 +1,68 @@
+package br.com.sus_online.controller;
+
+
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.sus_onlineDao.*;
+import br.com.sus_onlineDao.model.*;
+
+/**
+ * Servlet implementation class AjaxApp
+ */
+@WebServlet("/AjaxApp")
+public class AjaxApp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private DaoAjaxApp dao = new DaoAjaxApp();
+	
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxApp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+
+		if (action == null) { 
+			throw new ServletException("No action specified.");
+		} else if (action.equals("home_barra_superior")) {
+			BuscarLocal(request, response);
+		} 
+	}
+	
+	public void BuscarLocal(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		List<Object> locais;
+		try {
+			dao.getLocalUbs();
+			request.setAttribute("mensagem", "Agenda");
+			request.getRequestDispatcher("view/exibeAgenda.jsp").forward(request, response);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/br/com/sus_online/model/LocalUbs.java
@@ -0,0 +1,21 @@
+package br.com.sus_online.model;
+
+
+public class LocalUbs {
+	int idLocalUbs;
+	String nome;
+	
+	public int getIdLocalUbs() {
+		return idLocalUbs;
+	}
+	public void setIdLocalUbs(int idLocalUbs) {
+		this.idLocalUbs = idLocalUbs;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
+
